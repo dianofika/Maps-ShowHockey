05e744413fc62561608bab814e8e7f706cf7d3f2
dianofika <diannofika.dn@gmail.com>
2017-10-14 07:58:25 +0700

    Final Commit

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xiirpl210/showhockey/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xiirpl210/showhockey/MainActivity.java
@@ -1,13 +1,106 @@
 package id.sch.smktelkom_mlg.privateassignment.xiirpl210.showhockey;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    static final CameraPosition SEATTLE = CameraPosition.builder()
+            .target(new LatLng(47.6204, -122.2491))
+            .zoom(10)
+            .bearing(0)
+            .tilt(45)
+            .build();
+    GoogleMap m_map;
+    boolean mapReady = false;
+    MarkerOptions renton;
+    MarkerOptions kirkland;
+    MarkerOptions everett;
+    MarkerOptions lynnwood;
+    MarkerOptions montlake;
+    MarkerOptions kent;
+    MarkerOptions showare;
+
+    public Resources getResources() {
+        return super.getResources();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        renton = new MarkerOptions()
+                .position(new LatLng(47.489805, -122.120502))
+                .title("Renton");
+
+        kirkland = new MarkerOptions()
+                .position(new LatLng(47.7301985, -122.1768858))
+                .title("Kirkland");
+
+        everett = new MarkerOptions()
+                .position(new LatLng(47.978748, -122.202001))
+                .title("Everett");
+
+        lynnwood = new MarkerOptions()
+                .position(new LatLng(47.819533, -122.32288))
+                .title("Lynnwood");
+        montlake = new MarkerOptions()
+                .position(new LatLng(47.7973733, -122.3281771))
+                .title("Montlake Terrace");
+        kent = new MarkerOptions()
+                .position(new LatLng(47.385938, -122.258212))
+                .title("Kent Valley");
+        showare = new MarkerOptions()
+                .position(new LatLng(47.38702, -122.23986))
+                .title("Showare Center");
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mapReady = true;
+        m_map = map;
+        m_map.addMarker(renton);
+        m_map.addMarker(kirkland);
+        m_map.addMarker(everett);
+        m_map.addMarker(lynnwood);
+        m_map.addMarker(montlake);
+        m_map.addMarker(kent);
+        m_map.addMarker(showare);
+        flyTo(SEATTLE);
+    }
+
+    private void flyTo(CameraPosition target) {
+        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
     }
 }
